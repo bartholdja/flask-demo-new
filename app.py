d10d6f68612229e773c5703d101115d306d816d6
Julia Barthold <julia.barthold(at)zoo.ox.ac.uk>
2015-09-08 15:53:01 +0200

    debugging index() POST request part

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def index():
 		p.line(x, y, legend="Temp.", line_width=2)
 
 		# show the results
-		return show(p)	
+		show(p)	
 		#r = requests.get(url)
 		#dat1 = json.loads(r.text)
 		#df = pd.DataFrame(dat1['dataset']['data'], columns=dat1['dataset']['column_names'])
