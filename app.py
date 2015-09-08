5de1a7534c06610180fe6db23acc9d43dd043dfd
Julia Barthold <julia.barthold(at)zoo.ox.ac.uk>
2015-09-08 20:20:00 +0200

    changed the rendering

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask,render_template, redirect, request
-from bokeh.charts import TimeSeries, output_file, show
-from bokeh.plotting import figure, output_server("line")
+from flask import Flask,render_template, render_template, redirect, request
+from bokeh.charts import TimeSeries
+from bokeh.plotting import figure
+from bokeh.embed import components
+from bokeh.resources import INLINE
+from bokeh.templates import RESOURCES
+from bokeh.util.string import encode_utf8
 import requests as requests
 import pandas as pd
 import simplejson as json
 
-
+#from plots import build_plot
 app = Flask(__name__)
 
 app.vars={}
@@ -25,35 +29,32 @@ def index():
 		
 		url = 'https://www.quandl.com/api/v3/datasets/WIKI/' + str(app.vars['ticker']) + '.json?order=asc&rows=31&start_date=2015-08-01&end_date=2015-08-31'
 
-		x = [1, 2, 3, 4, 5]
-		y = [6, 7, 8, 9, 10]
+		r = requests.get(url)
+		dat1 = json.loads(r.text)
+		df = pd.DataFrame(dat1['dataset']['data'], columns=dat1['dataset']['column_names'])
+		df['Date'] = pd.to_datetime(df['Date'])
 
-		# output to static HTML file
-		#output_file("lines.html", title="line plot example")
-		output_server("line")
+		if app.vars['feature'] == 'Close':
+			ylab = "Closing price"
+		if app.vars['feature'] == 'Adj. Close':
+			ylab = "Adjusted closing price"
+		if app.vars['feature'] == 'Volume':
+			ylab = "Volume"
+		fig = TimeSeries(df[str(app.vars['feature'])], df['Date'], title=str(app.vars['ticker']), ylabel=ylab)
+		
+      	resources = RESOURCES.render(
+        js_raw=INLINE.js_raw,
+        css_raw=INLINE.css_raw,
+        js_files=INLINE.js_files,
+        css_files=INLINE.css_files,
+        )
 
-		# create a new plot with a title and axis labels
-		p = figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+        script, div = components(fig, INLINE)
+        html = render_template(
+		'index_post.html',
+		plot_script=script, plot_div=div, plot_resources=resources)
+	return encode_utf8(html)
 
-		# add a line renderer with legend and line thickness
-		p.line(x, y, legend="Temp.", line_width=2)
-
-		# show the results
-		return show(p)
-		#r = requests.get(url)
-		#dat1 = json.loads(r.text)
-		#df = pd.DataFrame(dat1['dataset']['data'], columns=dat1['dataset']['column_names'])
-		#df['Date'] = pd.to_datetime(df['Date'])
-
-		#output_file("prices.html", title="stock prices")
-		#if app.vars['feature'] == 'Close':
-	#		ylab = "Closing price"
-	#	if app.vars['feature'] == 'Adj. Close':
-	#		ylab = "Adjusted closing price"
-	#	if app.vars['feature'] == 'Volume':
-	#		ylab = "Volume"
-	#	p = TimeSeries(df[str(app.vars['feature'])], df['Date'], title=str(app.vars['ticker']), ylabel=ylab)
-
-	#	return show(p)
 if __name__ == '__main__':
     app.run(debug=True)
+	
